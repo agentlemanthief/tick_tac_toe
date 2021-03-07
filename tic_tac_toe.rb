32e6787be7ccc7bc774ee58ae089ae058594c02b
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-03-07 12:48:45 +0000

    Minor formatting change

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -12,7 +12,7 @@ class GameBoard
   end
 
   def print_board()
-    puts "\n  1  2  3\na|#{@board[0][0]}  #{@board[0][1]}  #{@board[0][2]}\nb|#{@board[1][0]}  #{@board[1][1]}  #{@board[1][2]}\nc|#{@board[2][0]}  #{@board[2][1]}  #{@board[2][2]}\n"
+    puts "\n  1  2  3\na|#{@board[0][0]}  #{@board[0][1]}  #{@board[0][2]}\nb|#{@board[1][0]}  #{@board[1][1]}  #{@board[1][2]}\nc|#{@board[2][0]}  #{@board[2][1]}  #{@board[2][2]}\n\n"
   end
 
   def place_o_or_x(co_ord, o_or_x)
